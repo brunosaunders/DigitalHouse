73a0f730a7549b4738565138336e55255d280c38
Bruno Saunders <senabruno97@hotmail.com>
2020-11-13 23:26:28 -0300

    hiding keyboard when screen has changed

--- Recipes/app/src/main/java/com/example/recipes/LoginFragment/LoginFragment.kt
+++ Recipes/app/src/main/java/com/example/recipes/LoginFragment/LoginFragment.kt
@@ -8,6 +8,7 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.recipes.R
+import com.example.recipes.util.hideKeyboard
 import com.example.recipes.util.hideKeyboardOnScrolled
 import kotlinx.android.synthetic.main.fragment_login.view.*
 
@@ -22,15 +23,16 @@ class LoginFragment : Fragment() {
 
         view.findViewById<Button>(R.id.button_register_LoginFragment).setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+            hideKeyboard(view)
         }
 
         view.button_logIn_LoginFragment.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+            hideKeyboard(view)
         }
 
         hideKeyboardOnScrolled(view.findViewById(R.id.scrollView_LoginFragment))
         return view
     }
-
 
 }
--- Recipes/app/src/main/java/com/example/recipes/util/Utils.kt
+++ Recipes/app/src/main/java/com/example/recipes/util/Utils.kt
@@ -1,6 +1,7 @@
 package com.example.recipes.util
 
 import android.content.Context
+import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.ScrollView
 import com.example.recipes.R
@@ -17,6 +18,11 @@ fun hideKeyboardOnScrolled(scrollView: ScrollView) {
             }
         }
     }
+}
+
+fun hideKeyboard(view: View) {
+    val imm: InputMethodManager = view.context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
 }
 
 fun getRestaurantList(): List<Restaurant> {
