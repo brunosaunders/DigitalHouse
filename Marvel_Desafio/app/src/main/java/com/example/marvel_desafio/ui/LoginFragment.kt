bbd6d45be50ef4cb346cb19a13b27a3d7b03288d
Bruno Saunders <senabruno97@hotmail.com>
2020-11-28 18:17:11 -0300

    Login screen

---
+++ Marvel_Desafio/app/src/main/java/com/example/marvel_desafio/ui/LoginFragment.kt
@@ -0,0 +1,25 @@
+package com.example.marvel_desafio.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.marvel_desafio.R
+import com.example.marvel_desafio.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        return binding.root
+    }
+
+}
